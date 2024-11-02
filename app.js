1556dbd95a65e566a06ef64f3957393d3e1235d4
Geethya Tennakoon <36676130+geethya1995@users.noreply.github.com>
2024-11-02 14:13:28 +1100

    feat(app): handle CORS errors

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ const connectDB = require("./config/db");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Handle CORS errors
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 // connecting to the database
 connectDB();
 
