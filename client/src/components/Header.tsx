7d384710bb6da67d4a43e188521c69e44eac0ea6
Geethya Tennakoon <36676130+geethya1995@users.noreply.github.com>
2024-11-03 21:13:21 +1100

    fix(client): fix vercel deployment issue - removed unused libs

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,8 @@
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import logo from "../assests/SportsBetLogo.jpg";
-import * as React from "react";
-
-// import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
